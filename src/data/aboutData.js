836d8e5269e205d306e21a7a264490b86631c36f
ma9da <dodymasry25@gmail.com>
2022-05-06 16:58:06 +0200

    editing a typo

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "Hi, I'm Magda Ahmed. I'm Front-End/UI Web Developer.",
+    description1: "Hi, I'm Magda Ahmed. a Front-End/UI Web Developer.",
     description2:"I create Front-End/UI for websites using different tools and languages/frameworks, my top priorities in any project that I'm working on are Website Accessibility and Responsiveness. I can help you grow your local business by providing you a great website for your online presence to share information and links about you or maybe show off your services and products etc.",
     image: 2
 }
