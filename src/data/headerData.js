e36546b3bc95b18f3867e8f4bc56bbdc8e98a198
ma9da <dodymasry25@gmail.com>
2022-05-06 15:39:14 +0200

    adding describtion

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import cv from '../assets/png/cv.png'
 export const headerData = {
     name: 'Magda Ahmed',
     title: "Front-End/UI Web Developer",
-    desciption:"Life may be a meandering path but don't worry about it, rather enjoy it. It is about the journey, not the destination so explore all that comes your way, learn and grow from it, for this journey only happens once.",
+    desciption:"For my position I like both the design aspect because it helps me unleash my creative side while the technical aspect helps me to build logic and solve problems so I find myself enjoying doing both.Working in IT makes it impossible to get enough or stop learning, so I'll always consider myself a full-time student.",
     image: cv,
     resumePdf: resume
 }
